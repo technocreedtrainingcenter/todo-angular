de0b712217a00f17f3e46ecaebd4de8d95ef9ccb
avinashgholap <avinashgholap123@gmail.com>
2021-07-28 20:39:19 +0530

    Service itegrated

--- src/app/components/todo/todo-component.ts
+++ src/app/components/todo/todo-component.ts
@@ -1,18 +1,27 @@
 import { Component, Input, EventEmitter, Output } from "@angular/core";
+import { TodoService } from "src/app/services/todo.service";
 
 @Component({
     selector: 'app-todo',
     templateUrl: './todo-component.html'
 })
 export class TODOComponent {
+    constructor(
+        private todoService: TodoService
+    ) {
+
+    }
+
     @Input() todoText: any;
 
     @Input() index: number = 0;
 
-    @Output() deleteTodoEvent = new EventEmitter();
+    // @Output() deleteTodoEvent = new EventEmitter();
     
     deleteTodo() {
-        this.deleteTodoEvent.emit(this.index);
+        debugger
+        this.todoService.deleteTodo.next(this.index);
+        // this.deleteTodoEvent.emit(this.index);
     }
 
 }
